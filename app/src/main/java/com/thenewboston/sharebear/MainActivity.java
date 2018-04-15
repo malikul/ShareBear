90ea65b061b41eb73762a057e17d20cb285e3360
Malikul <malikul@users.noreply.github.com>
2018-04-15 14:40:54 +0700

    Initial commit

---
+++ app/src/main/java/com/thenewboston/sharebear/MainActivity.java
@@ -0,0 +1,50 @@
+package com.thenewboston.sharebear;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    EditText passwordInput;
+    TextView buckysText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = (EditText) findViewById(R.id.usernameInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+        buckysText = (TextView) findViewById(R.id.buckysText);
+    }
+
+    // Save the users login info
+    public void saveInfoClick(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("username", usernameInput.getText().toString());
+        editor.putString("password", passwordInput.getText().toString());
+        editor.apply();
+
+        Toast.makeText(this, "Saved!", Toast.LENGTH_LONG).show();
+    }
+
+    // Print out the saved data
+    public void displayInfoClick(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        String name = sharedPref.getString("username", "");
+        String pv = sharedPref.getString("password", "");
+
+        buckysText.setText(name + " " + pv);
+    }
+}
